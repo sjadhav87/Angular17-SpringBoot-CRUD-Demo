352b7cbb22282ae10c6f3d651d5abf87ceb09117
Shital Pawar <sjadhav87@users.noreply.github.com>
2024-10-07 16:38:54 +0200

    Initail Commit

---
+++ springboot-backend-code/src/main/java/com/sap/employee_management/SpringbootAngularDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sap.employee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularDemoProjectApplication.class, args);
+	}
+
+}
---
+++ springboot-backend-code/src/test/java/com/sap/employee_management/SpringbootAngularDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
